4447827bf7fcd03f336fcd6d201e1ee7fda6d79b
Robert Kujawa <robert.kujawa@proton.me>
2023-09-04 14:22:23 -0600

    Add service stub publishing test

--- tests/Feature/Console/Commands/PublishStubsCommandTest.php
+++ tests/Feature/Console/Commands/PublishStubsCommandTest.php
@@ -18,4 +18,18 @@ class PublishStubsCommandTest extends TestCase
             $this->assertFileExists(base_path('stubs/serviceable/' . $stub . '.stub'));
         }
     }
+
+    /** @test */
+    public function publish_stubs_command_publishes_stubs_for_service()
+    {
+        $this->artisan('service:stubs', [
+            '--service' => 'mock',
+        ])
+            ->expectsOutput('Successfully published stubs!')
+            ->assertExitCode(0);
+
+        foreach(PublishStubs::$serviceStubs as $stub) {
+            $this->assertFileExists(base_path('stubs/serviceable/mock/' . $stub . '.stub'));
+        }
+    }
 }
