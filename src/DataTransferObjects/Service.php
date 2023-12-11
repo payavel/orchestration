c1f1d9160bd5efaac814938c41f6b6a5cf60a33e
Robert Kujawa <robert.kujawa@proton.me>
2023-12-11 00:35:26 -0600

    Implement getDefault methods in config driver

--- src/DataTransferObjects/Service.php
+++ src/DataTransferObjects/Service.php
@@ -2,6 +2,7 @@
 
 namespace Payavel\Orchestration\DataTransferObjects;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Str;
 use Payavel\Orchestration\Contracts\Serviceable;
 use Payavel\Orchestration\Traits\SimulatesAttributes;
@@ -33,5 +34,19 @@ class Service implements Serviceable
     public function getName()
     {
         return Str::headline($this->attributes['id']);
+    }
+
+    /**
+     * Create a new static instance from a serviceable.
+     *
+     * @param Serviceable $service
+     * @return static
+     */
+    public static function fromServiceable(Serviceable $service)
+    {
+        return new static(array_merge(
+            ['id' => $service->getId()],
+            Config::get('orchestration.services.' . $service->getId(), [])
+        ));
     }
 }
