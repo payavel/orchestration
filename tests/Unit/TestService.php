c96e6c771e84ca48563ba02f57fabdbe2f80da2c
r-kujawa <robert.kujawa@proton.me>
2024-01-20 16:02:14 -0600

    Re-SetsMode

--- tests/Unit/TestService.php
+++ tests/Unit/TestService.php
@@ -11,12 +11,9 @@ use Payavel\Orchestration\Tests\Contracts\CreatesServiceables;
 use Payavel\Orchestration\Tests\Services\Mock\FakeMockRequest;
 use Payavel\Orchestration\Tests\Services\Mock\TestMockRequest;
 use Payavel\Orchestration\Tests\TestCase;
-use Payavel\Orchestration\Tests\Traits\SetsMode;
 
 abstract class TestService extends TestCase implements CreatesServiceables
 {
-    use SetsMode;
-
     private $serviceable;
     private $providable;
     private $merchantable;
@@ -45,82 +42,107 @@ abstract class TestService extends TestCase implements CreatesServiceables
 
         $this->setDefaultsForService($this->serviceable, $this->merchantable, $this->providable);
 
-        $this->setMode($this->serviceable);
-
         $this->service = new Service($this->serviceable);
+    }
+
+    public function assertRealIsAlignedWithFake(callable $test)
+    {
+        $config = Str::slug($this->serviceable->getId());
+
+        Config::set($config . '.test_mode', false);
+
+        $test();
+
+        Config::set($config . '.test_mode', true);
+
+        $this->service->reset();
+
+        $test();
     }
 
     /** @test */
     public function set_provider_and_merchant_fluently()
     {
-        $response = $this->service
+        $this->assertRealIsAlignedWithFake(function () {
+            $response = $this->service
             ->provider($this->providable)
             ->merchant($this->merchantable)
             ->getIdentity();
 
-        $this->assertEquals(
-            Config::get(Str::slug($this->serviceable->getId()) . '.test_mode')
-                ? 'Fake'
-                : 'Real',
-            $response->data
-        );
+            $this->assertEquals(
+                Config::get(Str::slug($this->serviceable->getId()) . '.test_mode')
+                    ? 'Fake'
+                    : 'Real',
+                $response->data
+            );
 
-        $this->assertResponseIsConfigured($response);
+            $this->assertResponseIsConfigured($response);
+        });
     }
 
     /** @test */
     public function setting_invalid_driver_throws_exception()
     {
-        Config::set(Str::slug($this->serviceable->getId()) . '.defaults.driver', 'invalid');
+        $this->assertRealIsAlignedWithFake(function () {
+            Config::set(Str::slug($this->serviceable->getId()) . '.defaults.driver', 'invalid');
 
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage('Invalid driver provided.');
+            $this->expectException(Exception::class);
+            $this->expectExceptionMessage('Invalid driver provided.');
 
-        $service = new Service($this->serviceable);
+            $service = new Service($this->serviceable);
+        });
     }
 
     /** @test */
     public function setting_invalid_provider_throws_exception()
     {
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage('Invalid provider.');
+        $this->assertRealIsAlignedWithFake(function () {
+            $this->expectException(Exception::class);
+            $this->expectExceptionMessage('Invalid provider.');
 
-        $this->service->setProvider('invalid');
+            $this->service->setProvider('invalid');
+        });
     }
 
     /** @test */
     public function setting_invalid_merchant_throws_exception()
     {
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage('Invalid merchant.');
+        $this->assertRealIsAlignedWithFake(function () {
+            $this->expectException(Exception::class);
+            $this->expectExceptionMessage('Invalid merchant.');
 
-        $this->service->setMerchant('invalid');
+            $this->service->setMerchant('invalid');
+        });
     }
 
     /** @test */
     public function setting_incompatible_merchant_provider_throws_exception()
     {
-        $incompatibleMerchant = $this->createMerchant($this->serviceable);
+        $this->assertRealIsAlignedWithFake(function () {
+            $incompatibleMerchant = $this->createMerchant($this->serviceable);
 
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage('The ' . $incompatibleMerchant->getName() . ' merchant is not supported by the ' . $this->providable->getName() . ' provider.');
+            $this->expectException(Exception::class);
+            $this->expectExceptionMessage('The ' . $incompatibleMerchant->getName() . ' merchant is not supported by the ' . $this->providable->getName() . ' provider.');
 
-        $this->service->provider($this->providable)->merchant($incompatibleMerchant)->getIdentity();
+            $this->service->provider($this->providable)->merchant($incompatibleMerchant)->getIdentity();
+        });
     }
 
     /** @test */
     public function resetting_service_to_default_configuration()
     {
-        $alternativeMerchant = $this->createMerchant($this->serviceable);
-        $this->linkMerchantToProvider($alternativeMerchant, $this->providable);
+        $this->assertRealIsAlignedWithFake(function () {
+            $alternativeMerchant = $this->createMerchant($this->serviceable);
+            $this->linkMerchantToProvider($alternativeMerchant, $this->providable);
 
-        $this->service->provider($this->providable)->merchant($alternativeMerchant);
+            $this->service->provider($this->providable)->merchant($alternativeMerchant);
 
-        $this->assertEquals($alternativeMerchant->getId(), $this->service->getIdentity()->merchant->getId());
+            $this->assertEquals($alternativeMerchant->getId(), $this->service->getIdentity()->merchant->getId());
 
-        $this->service->reset();
+            $this->service->reset();
 
-        $this->assertEquals($this->merchantable->getId(), $this->service->getIdentity()->merchant->getId());
+            $this->assertEquals($this->merchantable->getId(), $this->service->getIdentity()->merchant->getId());
+        });
     }
 
     protected function assertResponseIsConfigured(ServiceResponse $response)
