bc8851bdfb4c82d3614f162141af0b405e720e10
r-kujawa <robert.kujawa@proton.me>
2024-01-22 00:20:14 -0600

    Make database driver specific assertions

--- tests/Feature/Console/Commands/DatabaseOrchestrateServiceCommandTest.php
+++ tests/Feature/Console/Commands/DatabaseOrchestrateServiceCommandTest.php
@@ -2,6 +2,13 @@
 
 namespace Payavel\Orchestration\Tests\Feature\Console\Commands;
 
+use Illuminate\Support\Str;
+use Payavel\Orchestration\Contracts\Merchantable;
+use Payavel\Orchestration\Contracts\Providable;
+use Payavel\Orchestration\Contracts\Serviceable;
+use Payavel\Orchestration\Models\Merchant;
+use Payavel\Orchestration\Models\Provider;
+use Payavel\Orchestration\Models\Service;
 use Payavel\Orchestration\Tests\Traits\CreatesDatabaseServiceables;
 use Payavel\Orchestration\Tests\Traits\SetsDatabaseDriver;
 
@@ -9,4 +16,60 @@ class DatabaseOrchestrateServiceCommandTest extends TestOrchestrateServiceComman
 {
     use CreatesDatabaseServiceables,
         SetsDatabaseDriver;
+
+    /**
+     * Determines if the generated migration has already been executed.
+     *
+     * @var boolean
+     */
+    private bool $migrated = false;
+
+    protected function makeSureProviderExists(Serviceable $service, Providable $provider)
+    {
+        $this->migrate($service);
+
+        $provider = Provider::find($provider->getId());
+
+        $this->assertNotNull($provider);
+        $this->assertEquals(
+            'App\\Services\\' . Str::studly($service->getId()) . '\\' . Str::studly($provider->getId()) . Str::studly($service->getId()) . 'Request',
+            $provider->gateway
+        );
+    }
+
+    protected function makeSureMerchantExists(Serviceable $service, Merchantable $merchant)
+    {
+        $this->migrate($service);
+
+        $merchant = Merchant::find($merchant->getId());
+
+        $this->assertNotNull($merchant);
+        $this->assertNotEmpty($merchant->providers);
+    }
+
+    protected function makeSureProviderIsLinkedToMerchant(Serviceable $service, Providable $provider, Merchantable $merchant)
+    {
+        $this->migrate($service);
+
+        $provider = Provider::find($provider->getId());
+        $merchant = Merchant::find($merchant->getId());
+        
+        $this->assertNotNull($provider->merchants()->where('merchants.id', $merchant->id)->first());
+        $this->assertNotNull($merchant->providers()->where('providers.id', $provider->id)->first());
+    }
+
+    private function migrate(Serviceable $service)
+    {
+        if ($this->migrated) {
+            return;
+        }
+
+        Merchant::where('service_id', $service->getId())->delete();
+        Provider::where('service_id', $service->getId())->delete();
+        Service::where('id', $service->getId())->delete();
+
+        $this->artisan('migrate');
+
+        $this->migrated = true;
+    }
 }
