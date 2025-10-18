7ecc6fe6e0b06b496f39f0fe373529e520982933
Robert Kujawa <robert.kujawa@proton.me>
2025-10-18 13:34:00 -0600

    Specify the full class path

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -34,7 +34,7 @@ class AccountFactory extends Factory
     /**
      * Configure the model factory.
      *
-     * @return static
+     * @return \Payavel\Orchestration\Database\Factories\AccountFactory
      */
     public function configure(): static
     {
