92c1d82f70d307f8259d9124fd13a08810c263d1
Robert Kujawa <robert.kujawa@proton.me>
2025-10-18 13:18:38 -0600

    Fix magic typing

--- tests/Services/Mock/TestMockRequest.php
+++ tests/Services/Mock/TestMockRequest.php
@@ -3,12 +3,11 @@
 namespace Payavel\Orchestration\Tests\Services\Mock;
 
 use Payavel\Orchestration\ServiceRequest;
-use Payavel\Orchestration\ServiceResponse;
 use Payavel\Orchestration\Tests\Services\Mock\Contracts\MockRequester;
 
 class TestMockRequest extends ServiceRequest implements MockRequester
 {
-    public function getIdentity(bool $withAdditionalData = false): ServiceResponse
+    public function getIdentity(bool $withAdditionalData = false): mixed
     {
         $response = [];
 
