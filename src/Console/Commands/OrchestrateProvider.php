8308620a23a048f7b8128fa8cc2d5869c30cc992
Robert Kujawa <robert.kujawa@proton.me>
2025-10-18 10:40:33 -0600

    Explicitly set typing for commands

--- src/Console/Commands/OrchestrateProvider.php
+++ src/Console/Commands/OrchestrateProvider.php
@@ -23,48 +23,48 @@ class OrchestrateProvider extends Command
      *
      * @var string
      */
-    protected $signature = 'orchestrate:provider
-                            {provider? : The provider name}
-                            {--id= : The provider ID}
-                            {--service= : The service ID}
-                            {--fake : Generates a gateway to be used for testing purposes}';
+    protected string $signature = 'orchestrate:provider
+                                    {provider? : The provider name}
+                                    {--id= : The provider ID}
+                                    {--service= : The service ID}
+                                    {--fake : Generates a gateway to be used for testing purposes}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Scaffold a new service provider\'s gateway request and response classes.';
+    protected string $description = 'Scaffold a new service provider\'s gateway request and response classes.';
 
     /**
      * The provider's service config.
      *
      * @var \Payavel\Orchestration\ServiceConfig
      */
-    protected $serviceConfig;
+    protected ServiceConfig $serviceConfig;
 
     /**
      * The service provider's name.
      *
      * @var string
      */
-    protected $name;
+    protected string $name;
 
     /**
      * The service provider's id.
      *
      * @var string
      */
-    protected $id;
+    protected string $id;
 
     /**
-     * Execute the console command.
+     * Executes the console command.
      *
      * @return void
      */
-    public function handle()
+    public function handle(): void
     {
-        if(! $this->setProperties()) {
+        if (! $this->setProperties()) {
             return;
         }
 
@@ -72,11 +72,11 @@ class OrchestrateProvider extends Command
     }
 
     /**
-     * Format the service provider's properties.
+     * Formats the service provider's properties.
      *
      * @return bool
      */
-    protected function setProperties()
+    protected function setProperties(): bool
     {
         if (! $this->setService()) {
             return false;
@@ -97,11 +97,11 @@ class OrchestrateProvider extends Command
     }
 
     /**
-     * Generated the provider gateway files.
+     * Generates the provider gateway files.
      *
      * @return void
      */
-    protected function generateProvider()
+    protected function generateProvider(): void
     {
         $service = Str::studly($this->serviceConfig->id);
         $provider = Str::studly($this->id);
@@ -134,11 +134,11 @@ class OrchestrateProvider extends Command
     }
 
     /**
-     * Set the service property.
+     * Sets the service property.
      *
      * @return bool
      */
-    protected function setService()
+    protected function setService(): bool
     {
         if (! is_null($this->option('service')) && is_null($serviceConfig = ServiceConfig::find($this->option('service')))) {
             error("Service with id {$this->option('service')} does not exist.");
