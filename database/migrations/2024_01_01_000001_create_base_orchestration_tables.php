17e326338d6f5a290ad73768edae508fb07e0b96
Robert Kujawa <robert.kujawa@proton.me>
2026-06-09 05:57:58 -0600

    Remove config_format from providers table

--- database/migrations/2024_01_01_000001_create_base_orchestration_tables.php
+++ database/migrations/2024_01_01_000001_create_base_orchestration_tables.php
@@ -17,7 +17,6 @@ return new class () extends Migration {
             $table->string('name')->nullable();
             $table->string('service_id');
             $table->string('gateway');
-            $table->json('config_format')->nullable();
             $table->timestamps();
         });
 
